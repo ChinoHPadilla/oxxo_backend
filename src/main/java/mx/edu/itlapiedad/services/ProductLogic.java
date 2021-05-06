d499a0e68bf29cab92cfa83d8306e37ab78d046e
Fernando Villalobos <lobiitho@outlook.com>
2021-05-05 20:48:39 -0500

    Creacion de archivos ProductService y ProductLogic

--- src/main/java/mx/edu/itlapiedad/services/ProductLogic.java
+++ src/main/java/mx/edu/itlapiedad/services/ProductLogic.java
@@ -1,5 +1,32 @@
 package mx.edu.itlapiedad.services;
 
-public class ProductLogic {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mx.edu.itlapiedad.dao.ProductoDAO;
+import mx.edu.itlapiedad.models.Productos;
+@Service
+public class ProductosLogic implements ProductoService {
+	
+	@Autowired
+	ProductoDAO repositorio;
+	
+	@Override
+	public Productos buscar(int id) {
+		return repositorio.buscar(id);
+	}
+	
+	@Override
+	public Productos insertar(Productos producto) {
+		return repositorio.insertar(producto);
+	}
+	
+	@Override
+	public List<Productos> consultarProductos() {
+		return repositorio.consultarProductos();
+	}
 
 }
+	}
--- src/main/java/mx/edu/itlapiedad/services/ProductoService.java
+++ src/main/java/mx/edu/itlapiedad/services/ProductoService.java
@@ -1,5 +1,16 @@
 package mx.edu.itlapiedad.services;
 
+import java.util.List;
+
+import mx.edu.itlapiedad.models.Productos;
+
 public interface ProductoService {
+	
+	Productos insertar(Productos producto);
+	
+	List<Productos> consultarProductos();
+	
+	Productos buscar(int id);
+	
 
 }
